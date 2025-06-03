d16a5f9d0fd9b0ff1873f6918c95fd7849fdf913
ocurrentduke1 <a21110128@ceti.mx>
2025-06-02 21:27:04 -0600

    creation of project

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { LandingComponent } from './components/landing/landing.component';
+
+@Component({
+  selector: 'app-root',
+  imports: [LandingComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'Eclipse-app';
+}
